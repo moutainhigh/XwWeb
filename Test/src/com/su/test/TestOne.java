c602ba34c1d5beafed43b3052b25b9a3b072535f
su1573 <sph1573@163.com>
2018-06-10 20:03:49 +0800

    测试项目

---
+++ Test/src/com/su/test/TestOne.java
@@ -0,0 +1,14 @@
+package com.su.test;
+
+public class TestOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int  a = 60;
+//		a = a+=a-=a+a;
+//		System.out.println(a);
+//		System.out.println(!(5%2));
+
+	}
+
+}
